168517c5ac87b0b7d17d564996ad7cd46e15d39d
Yara Alkassar <yara.sarmad@yahoo.com>
2020-05-16 02:03:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,4 +8,11 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(t){
+
+  
+}
 /* Write your implementation of displayMessage() */
+function displayMessage() {
+  
+}
