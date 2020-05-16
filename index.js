765b5a0bfe83726db86513d6082a450f7ca9aa4e
Yara Alkassar <yara.sarmad@yahoo.com>
2020-05-16 03:29:17 +0000

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,21 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time){
+let h = time.split(':');
+if(h[0]>=0&&h[0]<12)
+return ("Good Morning");
+else if(h[0]>=12&&h[0]<18)
+{
+  return ("Good Afternoon");
+}
+else {
+  return ("Good Evening");
+}
+
+}
 /* Write your implementation of displayMessage() */
+
+function displayMessage(txt) {
+  document.getElementById('greeting').innerText = txt;
+}
